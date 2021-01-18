579673ac778fe22bc753326c841f194778318a8c
yuki <yuu.maru.571718@gmail.com>
2021-01-18 09:47:44 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,12 +46,14 @@ class TasksController < ApplicationController
     flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
-end
-
-private
+  
+  private
 
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
   end
+end
+
+
 
