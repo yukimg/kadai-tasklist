8ae2f2adefecd51ebd3e9dd9a4b66066d80f3ee9
yuki <yuu.maru.571718@gmail.com>
2021-01-27 09:13:24 +0000

    himo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.new(task_params)
       
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました'
