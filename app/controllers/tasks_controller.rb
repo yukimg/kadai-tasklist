d5f58a1c3db89c025083692b87c270de0273b4a3
yuki <yuu.maru.571718@gmail.com>
2021-01-29 13:42:54 +0000

    taskcon

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
    before_action :require_user_logged_in
    before_action :set_task, only: [:show, :edit, :update, :destroy]
-   before_action :correct_user, only: [:new, :show,:edit, :update, :destroy, :create]
+   before_action :correct_user, only: [:show,:edit,:destroy,]
    
   
    def index
