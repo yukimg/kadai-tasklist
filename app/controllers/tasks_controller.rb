75681e77069c51bf5f5fc58f64e1c6d7a2d4f111
yuki <yuu.maru.571718@gmail.com>
2021-01-30 11:45:28 +0000

    task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
    before_action :require_user_logged_in
-   before_action :correct_user, only: [:show, :edit, :destroy]
+   before_action :correct_user, only: [:show, :edit, :destroy, :update]
    before_action :set_task, only: [:show, :edit, :update, :destroy]
    
   
