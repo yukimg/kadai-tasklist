becde7a7ee54dd58f70f089ee326d40f69ec07dc
yuki <yuu.maru.571718@gmail.com>
2021-01-30 11:09:32 +0000

    root

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -61,7 +61,7 @@ class TasksController < ApplicationController
    def correct_user
       @task = current_user.tasks.find_by(id: params[:id])
       unless @task
-      redirect_to login_url
+      redirect_to root_url
       end
    end 
 end
